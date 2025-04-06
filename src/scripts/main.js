e19cef2f0b7f3c0daf589ad57296809f46fa19de
inexorableace <rcsaunder@gmail.com>
2025-04-06 13:06:52 -0400

    fix positioning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
+// binary vars
 const numArray = ['01101101','01100001','01110010','01100001','01110100','01101000','01101111','01101110','01101001','01110100','01100101','01101101','01101101','01100001','01101110','01100001','01100111','01100101','01110010']
 const numContainer = document.getElementById('putBinary');
 const arrayCount = numArray.length - 1;
 let intervalCount = 0
+
+// grid vars and setup
+const cells = document.querySelectorAll('#grid > div');
+const colors = ['green', 'white', 'blue'];
+const shapes = ['topRight', 'topLeft', 'bottomRight', 'bottomLeft'];
+for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    let color = colors[Math.floor(Math.random()*colors.length)];
+    let shape = shapes[Math.floor(Math.random()*shapes.length)];
+    cell.classList.remove(...cell.classList)
+    cell.classList.add(color, shape)
+}
+
 setInterval(function(){
+    // binary
     if (intervalCount > arrayCount){
         intervalCount = 0
     }else if (intervalCount <= 7){
@@ -21,4 +36,14 @@ setInterval(function(){
         numContainer.innerHTML = numArray[intervalCount];
         intervalCount += 1;
     }
+
+    // grid
+    for (let i = 0; i < cells.length; i++) {
+        const cell = cells[i];
+        let color = colors[Math.floor(Math.random()*colors.length)];
+        let shape = shapes[Math.floor(Math.random()*shapes.length)];
+        cell.classList.remove(...cell.classList)
+        cell.classList.add(color, shape)
+    }
 }, 1000)
+
